3fce68652a890c7840b65312ff672d621f271be7
Santosh Yadav <santosh.yadav198613@gmail.com>
2017-12-07 09:10:55 +0530

    added Binding and built-in directives

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Employee } from './employee';
+
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
@@ -8,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeComponent {
   empName: string = 'Deepak';
+  lastName: string = 'Test';
+  empId: number = 1;
+  visible: boolean = false;
+  employeeList: Employee[] = [
+    { id: 1, name: 'Jay', address: 'Mumbai', dob: new Date('12-Nov-1986'), salary: 45000 },
+    { id: 2, name: 'Deepak', address: 'Mumbai', dob: new Date('12-Nov-1988'), salary: 55000 },
+    { id: 3, name: 'pushplatha', address: 'Mumbai', dob: new Date('12-Nov-1989'), salary: 65000 },
+    { id: 4, name: 'Shawn', address: 'Mumbai', dob: new Date('12-Nov-1987'), salary: 75000 },
+    { id: 5, name: 'Vijay', address: 'Mumbai', dob: new Date('12-Nov-1985'), salary: 85000 },
+  ];
+  empRole: string = 'Super Admin';
+
+  // employeeList : Array<Employee> = 
 
   constructor() { }
 
+
+  toggle() {
+    this.visible = !this.visible;
+  }
 }
 
 
---
+++ src/app/employee/employee.ts
@@ -0,0 +1,7 @@
+export class Employee {
+    id: number;
+    name: string;
+    dob: Date;
+    salary: number;
+    address: string;
+}
